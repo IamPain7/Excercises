13b6c425154149ea4c7467357cc6c048211a4451
IamPain7 <jjvirtudazo@gmail.com>
2020-10-27 14:20:56 -0600

    File added

---
+++ VariablesExcercise/VariablesExcercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariablesExcercise
+{
+
+    /*
+        Step 1: Declare and initialize variables for the following types: 
+        string 
+        int 
+        char 
+        bool 
+        double
+        decimal 
+
+
+        Step 2: Concatenate these variables in a Console.Writeline();
+        For example:
+        string dogName = "Ralph";
+        int dogAge = 10;    
+        Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old"
+
+
+
+        Step 3: Analyze any errors you might incur, if any, and research how to fix it. This is a prime opportunity to exemplify self-learning.
+
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

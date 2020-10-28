cf047760158815a53239ab349ff12b6832df6184
IamPain7 <jjvirtudazo@gmail.com>
2020-10-28 15:26:28 -0600

    File added

---
+++ ForLoopPractice/ForLoopPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Practice write loop 15 times
+
+
+
+
+            
+
+           
+        }
+    }
+}

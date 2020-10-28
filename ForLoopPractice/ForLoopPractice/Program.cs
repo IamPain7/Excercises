4582a215a347ca09b110cafa9f1afd366103632f
IamPain7 <jjvirtudazo@gmail.com>
2020-10-28 15:53:31 -0600

    File updated

--- ForLoopPractice/ForLoopPractice/Program.cs
+++ ForLoopPractice/ForLoopPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 
 namespace ForLoopPractice
 {
@@ -7,14 +8,162 @@ namespace ForLoopPractice
         static void Main(string[] args)
         {
 
-            // Practice write loop 15 times
+             //Practice write loop 15 times
+
+            for (int i = 0; i < 5; i++)
+             {
+
+
+            Console.WriteLine($" {i}");
+
+
+           }
+
+            for (int i=1; i<10; i++)
+           {
+
+          Console.WriteLine($" {i}");
+
+             }
+
+            for (int i=2; i<15; i++)
+            {
+                Console.WriteLine($" {i}");
+                
+            }
+
+
+            for (int i=3; i<20; i++)
+            {
+
+                Console.WriteLine($" {i}");
+
+
+            }
+
+            for (int i = 4; i < 20; i++)
+            {
+
+
+                Console.WriteLine($" {i}");
+
+            }
+
+
+            for (int i=  5; i < 30; i++)
+            {
+
+                Console.WriteLine($" {i}");
 
 
 
+            }
+         
+            for (int i= 6; i< 40; i++)
+            {
+               
 
+                Console.WriteLine($" { i}");
+
+            }
             
+            for (int i=7; i<50; ++i)
+            {
 
+                Console.WriteLine($"{i}");
+
+
+            }
+
+
+            for (int i=8; i<60; ++i)
+            {
+
+
+                Console.WriteLine($"{i}");
+
+            }
+
+
+
+            for (int i=9; i<70; i++)
+            {
+
+                Console.WriteLine($"{i}");
+
+
+            }
+
+
+            for (int i=10; i<80; i++)
+            {
+
+                Console.WriteLine($"{i}");
+
+
+            }
+
+
+            for (int i= 11; i<90; ++i)
+            {
+
+
+                Console.WriteLine($"{i}");
+
+
+            }
            
+
+            for (int i=12; i<100; ++i)
+            {
+
+
+                Console.WriteLine($"{i}");
+
+
+            }
+
+
+            for (int i = 13; i < 115; ++i)
+            {
+
+
+                Console.WriteLine($"{i}");
+
+
+            }
+
+
+            for(int i=14; i<120; ++i)
+            {
+
+                Console.WriteLine($"{i}");
+
+
+
+            }
+
+
+            for(int i=14; i<130; ++i)
+            {
+
+
+                Console.WriteLine($"{i}");
+
+
+
+            }
+
+
+
+            for (int i=15; i<140; ++i)
+            {
+
+
+
+                Console.WriteLine($"{i}");
+
+            }
         }
     }
 }

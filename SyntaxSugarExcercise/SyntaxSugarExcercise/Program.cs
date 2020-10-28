b8258cfd4d6b0b220baf3b3bed4ec840084b62e7
IamPain7 <jjvirtudazo@gmail.com>
2020-10-28 11:46:49 -0600

    Excercise added

---
+++ SyntaxSugarExcercise/SyntaxSugarExcercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SyntaxSugarExcercise
+{
+    class Program
+    {
+
+
+        /*
+         * Given the following code, change the code to implement inferred typing, string interpolation, and the ternary operator
+
+         * int answer = 4;
+            string response;
+            
+            if (answer < 9) 
+            {
+                response = answer + " is less than nine";
+                        }
+                else
+                {
+                   response = answer + “greater than or equal to nine”;
+                }
+                Inferred Typing Sample:
+                    
+                    Cheat code:
+                    var name= "Jj";
+
+                    Explicit typing Sample:
+                    string name = "Jj";
+
+                    Inline or ternary operator
+                    var answer=(myAge<19)$ " You are 19 years old" : " You are too young";
+
+
+                    Syntax sugar or string interpolation sample:
+                    Console.Writeline($"Hello,{} your pets name {}, and ")
+
+         */
+        static void Main(string[] args)
+        {
+
+            int answer = 4; //Data type, Variable and Value
+            String response = Console.ReadLine();
+
+
+
+         var result = (answer < 4) ? "Yes it is less than nine" : " 9 is greater than 4"; //ternary operator
+
+            Console.WriteLine($"The result is {result}"); //string interpolation
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

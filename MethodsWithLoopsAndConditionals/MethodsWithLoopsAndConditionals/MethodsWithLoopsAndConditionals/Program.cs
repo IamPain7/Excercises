f8f2dd7398407c94c492c9ea8ac63051f8a3ebf0
IamPain7 <jjvirtudazo@gmail.com>
2020-10-29 16:54:24 -0600

    file added

---
+++ MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+
+      public static int excercise(int a )
+        {
+            Console.WriteLine();
+
+            return a;
+        }
+    }
+}
